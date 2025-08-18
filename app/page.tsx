02ab41b97562b1a6706e0e8a7280dc09825a83f6
Syeda.Farheen <spongheen@gmail.com>
2025-08-18 16:18:14 +0500

    small fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import Navbar from '@/components/Navbar'
-import HeroSection from '@/components/HeroSection'
-import StatsSection from '@/components/StatsSection'
-import WhyExist from '@/components/WhyExist'
-import MapImpact from '@/components/MapImpact'
-import DonateCards from '@/components/DonateCards'
-import StudentStories from '@/components/StudentStories'
-import ImpactStats from '@/components/ImpactStats'
-import TestimonialsCarousel from '@/components/TestimonialsCarousel'
-import FAQAccordion from '@/components/FAQAccordion'
-import CTASection from '@/components/CTASection'
-import Footer from '@/components/Footer'
-import LifeAtHunehar from '@/components/LifeAtHunehar'
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import StatsSection from "@/components/StatsSection";
+import WhyExist from "@/components/WhyExist";
+import MapImpact from "@/components/MapImpact";
+import DonateCards from "@/components/DonateCards";
+import StudentStories from "@/components/StudentStories";
+import ImpactStats from "@/components/ImpactStats";
+import TestimonialsCarousel from "@/components/TestimonialsCarousel";
+import FAQAccordion from "@/components/FAQAccordion";
+import CTASection from "@/components/CTASection";
+import Footer from "@/components/footer";
+import LifeAtHunehar from "@/components/LifeAtHunehar";
 
 export default function Home() {
   return (
@@ -29,5 +29,5 @@ export default function Home() {
       <CTASection />
       <Footer />
     </main>
-  )
+  );
 }
