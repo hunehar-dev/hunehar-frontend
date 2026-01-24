7ce728e25e1e008da4adb73d2ea85fd1c98e01cd
Syeda.Farheen <spongheen@gmail.com>
2026-01-24 12:51:27 +0500

    updated school page + made it responsive

--- components/SchoolGallery.tsx
+++ components/SchoolGallery.tsx
@@ -3,76 +3,85 @@
 import React from "react";
 import { LayoutGrid } from "@/components/ui/layout-grid";
 
-const SkeletonOne = () => (
-  <div>
-    <p className="font-bold md:text-3xl text-xl text-white">
-      Learning Beyond Classrooms
-    </p>
-    <p className="text-neutral-200 mt-2 max-w-lg">
-      A safe environment where children learn with confidence and dignity.
-    </p>
-  </div>
-);
-
-const SkeletonTwo = () => (
-  <div>
-    <p className="font-bold md:text-3xl text-xl text-white">
-      Dedicated Teachers
-    </p>
-    <p className="text-neutral-200 mt-2 max-w-lg">
-      Our teachers guide students with patience, care, and commitment.
-    </p>
-  </div>
-);
-
-const SkeletonThree = () => (
-  <div>
-    <p className="font-bold md:text-3xl text-xl text-white">
-      Classroom Engagement
-    </p>
-    <p className="text-neutral-200 mt-2 max-w-lg">
-      Encouraging participation, curiosity, and confidence.
-    </p>
-  </div>
-);
-
-const SkeletonFour = () => (
-  <div>
-    <p className="font-bold md:text-3xl text-xl text-white">
-      Hope in Every Child
-    </p>
-    <p className="text-neutral-200 mt-2 max-w-lg">
-      Education that nurtures dreams and builds futures.
-    </p>
-  </div>
-);
-
 const cards = [
   {
     id: 1,
-    content: <SkeletonOne />,
-    className: "md:col-span-2",
+    className: "md:col-span-2 h-[340px]",
     thumbnail: "/images/school/Hunehar-Gate.webp",
   },
   {
     id: 2,
-    content: <SkeletonTwo />,
-    className: "col-span-1",
-    thumbnail: "/images/school/Hunehar-Halls.webp",
+    className: "h-[340px]",
+    thumbnail: "/images/school/Hunehar-Smiling.webp",
   },
   {
     id: 3,
-    content: <SkeletonThree />,
-    className: "col-span-1",
-    thumbnail: "public/images/school/Hunehar-Halls.webp",
+    className: "h-[340px]",
+    thumbnail: "/images/school/Hunehar-Assembly.webp",
   },
   {
     id: 4,
-    content: <SkeletonFour />,
-    className: "md:col-span-2",
-    thumbnail: "/images/school/Hunehar-Halls.webp",
+    className: "md:col-span-2 h-[340px]",
+    thumbnail: "/images/school/Hunehar-children.webp",
+  },
+
+  {
+    id: 5,
+    className: "h-[340px]",
+    thumbnail: "/images/school/Hunehar-children-playing.webp",
+  },
+  {
+    id: 6,
+    className: "h-[340px]",
+    thumbnail: "/images/school/Hunehar-Children-Playing2.webp",
+  },
+
+  {
+    id: 7,
+    className: "h-[340px]",
+    thumbnail: "/images/school/Hunehar-Classroom.webp",
+  },
+  {
+    id: 8,
+    className: "h-[340px]",
+    thumbnail: "/images/school/computer-lab.webp",
+  },
+
+  {
+    id: 9,
+    className: "md:col-span-2 h-[340px]",
+    thumbnail: "/images/school/Hunehar-Library.webp",
+  },
+  {
+    id: 10,
+    className: "h-[340px]",
+    thumbnail: "/images/school/Hunehar-Shelf.webp",
+  },
+
+  {
+    id: 11,
+    className: "h-[340px]",
+    thumbnail: "/images/school/Hunehar-Student.webp",
+  },
+  {
+    id: 12,
+    className: "h-[340px]",
+    thumbnail: "/images/school/Hunehar-Student3.webp",
+  },
+
+  {
+    id: 13,
+    className: "md:col-span-2 h-[340px]",
+    thumbnail: "/images/school/Hunehar-Students2.webp",
+  },
+  {
+    id: 14,
+    className: "h-[340px]",
+    thumbnail: "/images/school/Hunehar-Students4.webp",
   },
 ];
+
+
 
 export default function SchoolGallery() {
   return (
