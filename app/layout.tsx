ba8dd069b93f9186ce715156675279ddc2346bf2
Syeda.Farheen <spongheen@gmail.com>
2026-01-25 22:09:58 +0500

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -66,6 +67,7 @@ export default function RootLayout({
       <body className="font-sans" suppressHydrationWarning>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
